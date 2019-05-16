c24cd0c112180babd03bf72b709df837a00d33ad
Serra <47144890+Serrape@users.noreply.github.com>
2019-05-16 12:56:35 +0200

    Ld Kid's near the end with lot of new Assets

--- Ferret_Unity/Assets/Scripts/LD/OnTriggerCollider.cs
+++ Ferret_Unity/Assets/Scripts/LD/OnTriggerCollider.cs
@@ -6,6 +6,8 @@ using UnityEngine.Events;
 [RequireComponent(typeof(BoxCollider))]
 public class OnTriggerCollider : MonoBehaviour {
 
+    [SerializeField] bool m_isActiveAtTheStart = true;
+
 	[Header("Tag to trigger")]
 	[SerializeField] string m_tagName = "Player";
 
@@ -14,20 +16,31 @@ public class OnTriggerCollider : MonoBehaviour {
 	[SerializeField] UnityEvent m_onTriggerStay;
 	[SerializeField] UnityEvent m_onTriggerExit;
 
-	void OnTriggerEnter(Collider col){
-		if(col.tag == m_tagName){
-			m_onTriggerEnter.Invoke();
+    bool m_activeOnce = false;
+
+    void OnTriggerEnter(Collider col){
+		if(col.tag == m_tagName && !m_activeOnce && m_isActiveAtTheStart)
+        {
+            m_activeOnce = true;
+            m_onTriggerEnter.Invoke();
 		}
 	}
 	void OnTriggerStay(Collider col){
-		if(col.tag == m_tagName){
+		if(col.tag == m_tagName && m_isActiveAtTheStart)
+        {
 			m_onTriggerStay.Invoke();
 		}
 	}
 	void OnTriggerExit(Collider col){
-		if(col.tag == m_tagName){
+		if(col.tag == m_tagName && m_isActiveAtTheStart)
+        {
 			m_onTriggerExit.Invoke();
 		}
 	}
+
+    public void ActiveTrigger(bool b)
+    {
+        m_isActiveAtTheStart = b;
+    }
 	
 }
