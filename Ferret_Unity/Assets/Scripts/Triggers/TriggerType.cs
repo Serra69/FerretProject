321b48a2c1a0532d9d7790cd4cf97597fb8fae36
Laptop_Jérémy <jeremy.payre@live.fr>
2019-05-22 12:58:44 +0200

    Improve CrawlTrigger.cs & Add CrawlController.cs

---
+++ Ferret_Unity/Assets/Scripts/Triggers/TriggerType.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerType : MonoBehaviour {
+
+	[System.Serializable] public enum TriggerTypes {
+		Enter,
+		Exit
+	}
+	
+}
