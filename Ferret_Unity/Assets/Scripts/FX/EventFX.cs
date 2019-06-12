b66704f22751737b815bf1089274dc4acc1779b1
Laptop_Jérémy <jeremy.payre@live.fr>
2019-06-12 09:02:24 +0200

    Add EventFX.cs

---
+++ Ferret_Unity/Assets/Scripts/FX/EventFX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventFX : MonoBehaviour {
+
+	[SerializeField] GameObject m_objectFX;
+	[SerializeField] Transform m_fxTransform;
+
+	public void AddFX(){
+		if(m_fxTransform != null){
+			Level.AddFX(m_objectFX, m_fxTransform.position, m_fxTransform.rotation);
+		}else{
+			Level.AddFX(m_objectFX, transform.position, transform.rotation);
+		}
+	}
+	
+}
