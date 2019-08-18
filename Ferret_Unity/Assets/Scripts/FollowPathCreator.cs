bd1d11d94e8d7024d633de4b65acca4ef4afa29b
Jeremy_Laptop <jeremy.payre@live.fr>
2019-08-18 19:44:20 +0200

    Add UnityExtensions.ArrayDrawer

--- Ferret_Unity/Assets/Scripts/FollowPathCreator.cs
+++ Ferret_Unity/Assets/Scripts/FollowPathCreator.cs
@@ -4,14 +4,19 @@ using PathCreation;
 
 public class FollowPathCreator : MonoBehaviour {
 
-	[Header("Transform who follow the path")]
+	[Header("Path Creator variables")]
+	[SerializeField] EndOfPathInstruction m_endOfPathInstruction = EndOfPathInstruction.Stop;
+
+	[Header("Transform who follows the path")]
 	[SerializeField] Transform m_followTransform;
 
 	[Header("Speeds")]
-	public Speeds[] m_speeds = new Speeds[1];
+	[SerializeField] float m_startMoveSpeed = 1;
+	public Speeds[] m_speeds = new Speeds[0];
 	[Serializable] public class Speeds {
-		public float m_timer = 0;
+		public float m_timer = 1;
 		public float m_moveSpeed = 1;
+		[HideInInspector] public bool m_timerIsPassed = false;
 	}
 
 	[Header("Gizmos")]
@@ -29,19 +34,23 @@ public class FollowPathCreator : MonoBehaviour {
 
 	void Start(){
 		m_pathCreator = GetComponent<PathCreator>();
+		m_moveSpeed = m_startMoveSpeed;
 	}
 
 	void Update(){
 		m_timer += Time.deltaTime;
 		for (int i = 0, l = m_speeds.Length; i < l; ++i) {
-			if(m_timer >= m_speeds[i].m_timer){
-				m_moveSpeed = m_speeds[i].m_moveSpeed;
+			if(!m_speeds[i].m_timerIsPassed){
+				if(m_timer >= m_speeds[i].m_timer){
+					m_moveSpeed = m_speeds[i].m_moveSpeed;
+					m_speeds[i].m_timerIsPassed = true;
+				}
 			}
 		}
 
 		m_distanceTravelled += m_moveSpeed * Time.deltaTime;
-		m_followTransform.position = m_pathCreator.path.GetPointAtDistance(m_distanceTravelled);
-		m_followTransform.rotation = m_pathCreator.path.GetRotationAtDistance(m_distanceTravelled);
+		m_followTransform.position = m_pathCreator.path.GetPointAtDistance(m_distanceTravelled, m_endOfPathInstruction);
+		m_followTransform.rotation = m_pathCreator.path.GetRotationAtDistance(m_distanceTravelled, m_endOfPathInstruction);
 	}
 
 	void OnDrawGizmos(){
