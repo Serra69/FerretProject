8fa107365935185958cdba0c439f81144be189a3
Jeremy_Laptop <jeremy.payre@live.fr>
2019-08-24 10:59:34 +0200

    Implementation FollowPathCreator

---
+++ Ferret_Unity/Assets/Scripts/Editor/FollowPathEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(FollowPathCreator))]
+public class FollowPathEditor : Editor {
+
+	[SerializeField] public Color[] m_colors = new Color[5];
+
+	FollowPathCreator m_followPathCreator;
+
+	void OnEnable(){
+		if(m_followPathCreator == null){
+			m_followPathCreator = (FollowPathCreator)target;
+		}
+	}
+
+	public override void OnInspectorGUI(){
+		base.OnInspectorGUI();
+		// for (int i = 0, l = m_colors.Length; i < l; ++i){
+		// 	m_colors[i] = EditorGUILayout.ColorField(m_colors[i]);
+		// }
+	}
+
+}
--- Ferret_Unity/Assets/Scripts/FollowPathCreator.cs
+++ Ferret_Unity/Assets/Scripts/FollowPathCreator.cs
@@ -12,16 +12,27 @@ public class FollowPathCreator : MonoBehaviour {
 	[SerializeField] bool m_usePathAtStart = true;
 	[SerializeField] float m_delayToStartUsePath = 0;
 	[SerializeField] Transform m_followTransform;
+	[Space]
+	[SerializeField] bool m_usePathRotation = true;
 
 	[Header("Move speeds")]
 	[SerializeField] float m_startMoveSpeed = 1;
 	public MoveSpeeds[] m_moveSpeeds = new MoveSpeeds[0];
 	[Serializable] public class MoveSpeeds {
+		[Header("CREATION ASSISTANCE")]
 		[Tooltip("You can not modify the variable.")]
 		public int m_positionNumber;
+
+		[Tooltip("Change the name to make it easier for you to find yourself!")]
+		public string m_levelPart;
+		[Space]
+
+		[Header("VARIABLES")]
 		[Tooltip("Timer from last moveSpeeds timer.")]
 		public float m_timer = 1;
 		public float m_moveSpeed = 1;
+		public float m_timeToReachNewSpeed = 1;
+		public AnimationCurve m_changeSpeedCurve;
 	}
 
 	[Header("Gizmos")]
@@ -40,11 +51,14 @@ public class FollowPathCreator : MonoBehaviour {
 	float m_actualLastTimer = 0;
 	int m_moveSpeedNumber = 0;
 
-	float m_moveSpeed = 0;
+	float m_actualMoveSpeed = 0;
+
+	IEnumerator m_changeSpeedCorout;
+	bool m_changeSpeedCoroutIsRunning = false;
 
 	void Start(){
 		m_pathCreator = GetComponent<PathCreator>();
-		m_moveSpeed = m_startMoveSpeed;
+		m_actualMoveSpeed = m_startMoveSpeed;
 		StartCoroutine(WaitTimeToUsePath(m_usePathAtStart));
 	}
 
@@ -55,15 +69,31 @@ public class FollowPathCreator : MonoBehaviour {
 		m_timer += Time.deltaTime;
 		if(m_moveSpeedNumber < m_moveSpeeds.Length){
 			if(m_timer >= m_actualLastTimer + m_moveSpeeds[m_moveSpeedNumber].m_timer){
-				m_moveSpeed = m_moveSpeeds[m_moveSpeedNumber].m_moveSpeed;
+				// m_actualMoveSpeed = m_moveSpeeds[m_moveSpeedNumber].m_moveSpeed;
+
+				if(m_changeSpeedCorout != null){
+					StopCoroutine(m_changeSpeedCorout);
+					if(m_changeSpeedCoroutIsRunning){
+						Debug.LogWarning("Caution! The ChangeSpeedCorout was overridden by the other after her!");
+					}
+				}
+				m_changeSpeedCorout = ChangeMoveSpeedValue(m_moveSpeeds[m_moveSpeedNumber].m_moveSpeed, m_moveSpeeds[m_moveSpeedNumber].m_timeToReachNewSpeed, m_moveSpeeds[m_moveSpeedNumber].m_changeSpeedCurve);
+				StartCoroutine(m_changeSpeedCorout);
+
 				m_actualLastTimer += m_moveSpeeds[m_moveSpeedNumber].m_timer;
 				m_moveSpeedNumber ++;
 			}
 		}
 
-		m_distanceTravelled += m_moveSpeed * Time.deltaTime;
+		m_distanceTravelled += m_actualMoveSpeed * Time.deltaTime;
 		m_followTransform.position = m_pathCreator.path.GetPointAtDistance(m_distanceTravelled, m_endOfPathInstruction);
-		m_followTransform.rotation = m_pathCreator.path.GetRotationAtDistance(m_distanceTravelled, m_endOfPathInstruction);
+		if(m_usePathRotation){
+			m_followTransform.rotation = m_pathCreator.path.GetRotationAtDistance(m_distanceTravelled, m_endOfPathInstruction);
+		}
+	}
+
+	public void UsePath(bool usePath){
+		StartCoroutine(WaitTimeToUsePath(usePath));
 	}
 
 	IEnumerator WaitTimeToUsePath(bool usePath){
@@ -71,8 +101,19 @@ public class FollowPathCreator : MonoBehaviour {
 		m_usePath = usePath;
 	}
 
-	public void UsePath(bool usePath){
-		StartCoroutine(WaitTimeToUsePath(usePath));
+	IEnumerator ChangeMoveSpeedValue(float newSpeedValue, float timeToReachNewSpeed, AnimationCurve changeSpeedCurve)
+	{
+		m_changeSpeedCoroutIsRunning = true;
+		float distance = Mathf.Abs(m_actualMoveSpeed - newSpeedValue);
+		float vitesse = distance / timeToReachNewSpeed;
+		float moveFracJourney = new float();
+
+		while(m_actualMoveSpeed != newSpeedValue){
+			moveFracJourney += (Time.deltaTime) * vitesse / distance;
+			m_actualMoveSpeed = Mathf.Lerp(m_actualMoveSpeed, newSpeedValue, changeSpeedCurve.Evaluate(moveFracJourney));
+			yield return null;
+		}
+		m_changeSpeedCoroutIsRunning = false;
 	}
 
 	void OnDrawGizmos(){
